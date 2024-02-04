40badd7ad1b15dd62f6226eef09514a11e895c83
Aditya Sharma <adisharma.8356@gmail.com>
2024-02-04 17:36:23 +0530

    Created Login and Sigup, Using Normal AUth, Show Password SVG not working

--- app/src/main/java/com/example/yema/Login_page.kt
+++ app/src/main/java/com/example/yema/Login_page.kt
@@ -7,42 +7,56 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.view.MotionEvent
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import com.example.yema.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.Firebase
 import org.w3c.dom.Text
 
 class Login_page : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
+    private lateinit var auth:FirebaseAuth
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
+        val instance = FirebaseAuth.getInstance()
+        val loginButton = findViewById<Button>(R.id.login_btn)
 
+        val password_editText: EditText = findViewById(R.id.login_password)
+        val email_editText: EditText = findViewById(R.id.login_email)
+
+
+
+//        Intent
         val signup_btn_hyper: TextView = findViewById<TextView>(R.id.sign_up_btn)
-
         signup_btn_hyper.setOnClickListener{
             startActivity(Intent(this, signup_page::class.java))
         }
 
 //      Show Password
-        val editText: EditText = findViewById(R.id.login_password)
         val drawable: Drawable? = ContextCompat.getDrawable(this, R.drawable.show_password_eye)
 
         if (drawable != null) {
             val wrappedDrawable = DrawableCompat.wrap(drawable)
 
             wrappedDrawable.setBounds(0, 0, wrappedDrawable.intrinsicWidth, wrappedDrawable.intrinsicHeight)
-            editText.setCompoundDrawablesWithIntrinsicBounds(null, null, wrappedDrawable, null)
+            password_editText.setCompoundDrawablesWithIntrinsicBounds(null, null, wrappedDrawable, null)
 
             var isPasswordVisible = false
 
-            editText.setOnTouchListener { v, event ->
+            password_editText.setOnTouchListener { v, event ->
                 if (event.action == MotionEvent.ACTION_UP) {
-                    if (event.rawX >= (editText.right - wrappedDrawable.bounds.width())) {
-                        editText.inputType = if (isPasswordVisible) {
+                    if (event.rawX >= (password_editText.right - wrappedDrawable.bounds.width())) {
+                        password_editText.inputType = if (isPasswordVisible) {
                             InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
                         } else {
                             InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
@@ -55,11 +69,20 @@ class Login_page : AppCompatActivity() {
             }
         }
 
+        loginButton.setOnClickListener {
+            val email = email_editText.text.toString().trim()
+            val password = password_editText.text.toString().trim()
+            instance.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Login in Successful", Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+                else {
+                    Toast.makeText(this, "Error Login", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
-
-
-
-
-
 
 }
--- app/src/main/java/com/example/yema/signup_page.kt
+++ app/src/main/java/com/example/yema/signup_page.kt
@@ -6,31 +6,40 @@ import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
+import android.util.Patterns
 import android.view.MotionEvent
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import com.example.yema.databinding.ActivityMainBinding
+import com.example.yema.databinding.ActivitySignUpBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import java.util.regex.Pattern
+import kotlin.math.log
 
 class signup_page : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignUpBinding
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val signin_btn_hyper: TextView = findViewById<TextView>(R.id.sign_in_btn)
 
-        signin_btn_hyper.setOnClickListener{
-            startActivity(Intent(this, Login_page::class.java))
-        }
 
 
 //        Show Password
         val editText: EditText = findViewById(R.id.signup_password)
         val drawable: Drawable? = ContextCompat.getDrawable(this, R.drawable.show_password_eye)
-
         if (drawable != null) {
             val wrappedDrawable = DrawableCompat.wrap(drawable)
 
@@ -43,8 +52,10 @@ class signup_page : AppCompatActivity() {
                 if (event.action == MotionEvent.ACTION_UP) {
                     if (event.rawX >= (editText.right - wrappedDrawable.bounds.width())) {
                         editText.inputType = if (isPasswordVisible) {
+                            Log.d("Not Showing", "Not Showing")
                             InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
                         } else {
+                            Log.d("Showing", "Showing")
                             InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
                         }
                         isPasswordVisible = !isPasswordVisible
@@ -54,5 +65,73 @@ class signup_page : AppCompatActivity() {
                 false
             }
         }
+
+//        Normal Authentication
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.signupBtn.setOnClickListener{
+            val username = binding.signupUsername.text.toString()
+            val email = binding.signupEmail.text.toString()
+            val password = binding.signupPassword.text.toString()
+
+            if (checkAllFields()){
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "Account Created!!!",Toast.LENGTH_LONG).show()
+                        startActivity(Intent(this, Login_page::class.java))
+                    }
+                    else{
+                        Toast.makeText(this, "Network Error:(",Toast.LENGTH_LONG).show()
+                        Log.e("error:", it.exception.toString())
+                    }
+                }
+            }
+        }
+
+//        Intent to Login_page.kt
+        val signin_btn_hyper: TextView = findViewById<TextView>(R.id.sign_in_btn)
+        signin_btn_hyper.setOnClickListener{
+            startActivity(Intent(this, Login_page::class.java))
+        }
     }
+
+
+
+
+
+//    FUNCTIONS
+    private fun checkAllFields(): Boolean{
+        val email = binding.signupEmail.text.toString()
+
+//        Username
+//        if (binding.signupUsername.text.toString() == ""){
+//            Toast.makeText(this, "Can't be Empty",Toast.LENGTH_LONG).show()
+//            return false
+//        }
+//        Email
+        if (binding.signupEmail.text.toString() == ""){
+            Toast.makeText(this, "Can't be Empty",Toast.LENGTH_LONG).show()
+            return false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Check Format",Toast.LENGTH_LONG).show()
+            return false
+        }
+//        Password
+        if (binding.signupPassword.text.toString()==""){
+            Toast.makeText(this, "Can't be Empty",Toast.LENGTH_LONG).show()
+            return false
+        }
+        if (binding.signupPassword.length() <= 7){
+            Toast.makeText(this, "Password Aleast have 8 letters",Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        return true
+    }
+
+
+
 }
