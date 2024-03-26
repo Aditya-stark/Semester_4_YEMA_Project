23888aa87fe53d1f0ee36501c1e43580fcf90ecc
Penguin5681 <pranavsinha922@gmail.com>
2024-03-26 23:51:23 +0530

    Added a upload image feature

--- app/src/main/java/com/example/yema/ProfileFragment.kt
+++ app/src/main/java/com/example/yema/ProfileFragment.kt
@@ -1,22 +1,32 @@
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.yema.ImagePicker
 import com.example.yema.R
 import com.example.yema.SignOutListener // Import the correct package for SignOutListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import de.hdodenhof.circleimageview.CircleImageView
+import java.lang.ref.PhantomReference
 
 class ProfileFragment : Fragment() {
 
     private lateinit var signOutListener: SignOutListener // Declare SignOutListener
     private lateinit var profilePhoto: CircleImageView
+    private lateinit var storageReference: StorageReference
+    private lateinit var selectedImageUri: Uri
+    private lateinit var uploadButton: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,6 +35,7 @@ class ProfileFragment : Fragment() {
 
         val buttonSignOut = view.findViewById<Button>(R.id.mainSign)
         profilePhoto = view.findViewById(R.id.profile_photo_profile_page)
+        uploadButton = view.findViewById(R.id.uploadBTN)
         val optionArray = arrayOf("Google Drive", "Google Photos", "Gallery")
 
         profilePhoto.setOnClickListener {
@@ -43,6 +54,11 @@ class ProfileFragment : Fragment() {
                 }
             })
             builder.show()
+//            storageReference.putFile()
+        }
+
+        uploadButton.setOnClickListener {
+            uploadToFirebaseStorage("adi@gmail.com")
         }
 
         buttonSignOut.setOnClickListener {
@@ -58,6 +74,31 @@ class ProfileFragment : Fragment() {
             signOutListener = context
         } else {
             throw RuntimeException("$context must implement SignOutListener")
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        selectedImageUri =
+            ImagePicker.handleImagePickerResult(requireActivity(), requestCode, resultCode, data)!!
+        if (selectedImageUri != null) {
+            profilePhoto.setImageURI(selectedImageUri)
+        }
+        else {
+            Toast.makeText(requireActivity(), "Select a photo", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun uploadToFirebaseStorage(email: String) {
+        storageReference = FirebaseStorage.getInstance().getReference()
+        var childReference: StorageReference = storageReference.child("profile_images/" + email + ".jpg")
+
+        val uploadTask: UploadTask = childReference.putFile(selectedImageUri)
+        uploadTask.addOnSuccessListener {
+            Toast.makeText(requireActivity(), "Photo Uploaded", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            Toast.makeText(requireActivity(), it.message, Toast.LENGTH_LONG).show()
         }
     }
 
